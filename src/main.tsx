57702c20834b5fc9d61b02b9cbeed271ee15f86e
unknown <meatrue@gmail.com>
2025-01-20 19:19:57 +0300

    Added base url to a router

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import './assets/styles/tailwind.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename='/novecograd'>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
